4c76b694fdbe0bb11e5763815e363ae895dd1e73
gustavo <gustavo.olmedo.formosa@gmail.com>
2022-06-28 11:50:50 -0300

    add theme

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components/native";
 import { RestaurantScreen } from "./src/features/restaurants/screens/RestaurantsScreen";
+import { theme } from "./src/infraestructure/theme";
 
 export default function App() {
   return (
     <>
-      <RestaurantScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
---
+++ src/infraestructure/theme/spacing.js
@@ -0,0 +1,7 @@
+export const lineHeights = {
+  title: "28px",
+  copy: "20px",
+};
+
+// spaces you can put in between things
+export const space = ["0px", "4px", "8px", "16px", "32px", "64px"];
