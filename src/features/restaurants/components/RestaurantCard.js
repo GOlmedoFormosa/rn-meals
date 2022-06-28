3d7716f9e78ad3dc57bb0a713aebdf9c11bb1f49
gustavo <gustavo.olmedo.formosa@gmail.com>
2022-06-28 13:01:06 -0300

    add fonts and address

--- src/features/restaurants/components/RestaurantCard.js
+++ src/features/restaurants/components/RestaurantCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Card as RNPaperCard } from "react-native-paper";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import styled from "styled-components";
 
 const Card = styled(RNPaperCard)`
@@ -12,9 +12,19 @@ const CardCover = styled(RNPaperCard.Cover)`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const Title = styled(Text)`
-  color: ${(props) => props.theme.colors.ui.primary};
+const Info = styled(View)`
   padding: ${(props) => props.theme.space[3]};
+`;
+
+const Title = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 export const RestaurantCard = ({ restaurant = {} }) => {
@@ -24,7 +34,7 @@ export const RestaurantCard = ({ restaurant = {} }) => {
     photos = [
       "https://i.pinimg.com/originals/e3/2d/67/e32d67b771afab60e846a71dbab5b44c.jpg",
     ],
-    // address = "100 some random street",
+    address = "100 some random street",
     // isOpenNow = true,
     // rating = 4,
     // isClosedTemprarily,
@@ -32,7 +42,10 @@ export const RestaurantCard = ({ restaurant = {} }) => {
   return (
     <Card elevation={5}>
       <CardCover key={name} source={{ uri: photos ? photos[0] : "" }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Address>{address}</Address>
+      </Info>
     </Card>
   );
 };
