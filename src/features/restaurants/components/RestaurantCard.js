f2de0ad1967e59f04bfd657168bdc39bd6b0718e
gustavo <gustavo.olmedo.formosa@gmail.com>
2022-07-05 16:18:54 -0300

    clean-up restaurant card component

--- src/features/restaurants/components/RestaurantCard.js
+++ src/features/restaurants/components/RestaurantCard.js
@@ -1,7 +1,4 @@
 import React from "react";
-import { Card as RNPaperCard } from "react-native-paper";
-import { View, Image } from "react-native";
-import styled from "styled-components";
 import { SvgXml } from "react-native-svg";
 
 import { Spacer } from "../../../components/Spacer/SpacerComponent";
@@ -10,46 +7,16 @@ import { Text } from "../../../components/Typography/TypographyComponent";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
 
-const Card = styled(RNPaperCard)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const CardCover = styled(RNPaperCard.Cover)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const Info = styled(View)`
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const Section = styled(View)`
-  align-items: center;
-  flex-direction: row;
-`;
-
-const SectionEnd = styled(View)`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
-
-const Icon = styled(Image)`
-  width: 15px;
-  height: 15px;
-`;
-
-const Rating = styled(View)`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
-
-const Address = styled.Text`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
-  color: ${(props) => props.theme.colors.ui.primary};
-`;
+import {
+  Card,
+  CardCover,
+  Info,
+  Section,
+  Rating,
+  SectionEnd,
+  Icon,
+  Address,
+} from "./restaurantCard.styles";
 
 export const RestaurantCard = ({ restaurant = {} }) => {
   const {
---
+++ src/features/restaurants/components/restaurantCard.styles.js
@@ -0,0 +1,44 @@
+export const Card = styled(RNPaperCard)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+export const CardCover = styled(RNPaperCard.Cover)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+import { Card as RNPaperCard } from "react-native-paper";
+import { View, Image } from "react-native";
+import styled from "styled-components";
+
+export const Info = styled(View)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export const Section = styled(View)`
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const SectionEnd = styled(View)`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+export const Icon = styled(Image)`
+  width: 15px;
+  height: 15px;
+`;
+
+export const Rating = styled(View)`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+export const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
