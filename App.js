56a645724b28261932edeb075a71b7bf0977177c
gustavo <gustavo.olmedo.formosa@gmail.com>
2022-07-07 10:05:52 -0300

    setup navigator and refactor container to make it reusable

--- App.js
+++ App.js
@@ -6,8 +6,23 @@ import {
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
-import { RestaurantScreen } from "./src/features/restaurants/screens/RestaurantsScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/RestaurantsScreen";
 import { theme } from "./src/infraestructure/theme";
+
+const Tab = createBottomTabNavigator();
+const SettingsScreen = () => <></>;
+const MapScreen = () => <></>;
+const MyTabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+      <Tab.Screen name="Map" component={MapScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -24,7 +39,9 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantScreen />
+        <NavigationContainer>
+          <MyTabs />
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
--- src/features/restaurants/screens/RestaurantsScreen.js
+++ src/features/restaurants/screens/RestaurantsScreen.js
@@ -1,16 +1,12 @@
 import React from "react";
-import { SafeAreaView, View, StatusBar, FlatList } from "react-native";
+import { View, FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 import styled from "styled-components";
 
 // restaurant components
+import { Container } from "../../../components/Container/ContainerComponent";
 import { Spacer } from "../../../components/Spacer/SpacerComponent";
 import { RestaurantCard } from "../components/RestaurantCard";
-
-const Container = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
 
 const SearchContainer = styled(View)`
   padding: ${(props) => props.theme.space[3]};
@@ -22,7 +18,7 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``;
 
-export const RestaurantScreen = () => {
+export const RestaurantsScreen = () => {
   return (
     <Container>
       <SearchContainer>
