10d1f6f70ed2c983357cda12cf77785cb6ddb124
gustavo <gustavo.olmedo.formosa@gmail.com>
2022-07-08 16:49:36 -0300

    add loader

--- src/features/restaurants/screens/RestaurantsScreen.js
+++ src/features/restaurants/screens/RestaurantsScreen.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { View, FlatList } from "react-native";
-import { Searchbar } from "react-native-paper";
+import { Searchbar, ActivityIndicator, Colors } from "react-native-paper";
 import styled from "styled-components";
 
 // restaurant components
@@ -8,6 +8,7 @@ import { Container } from "../../../components/Container/ContainerComponent";
 import { Spacer } from "../../../components/Spacer/SpacerComponent";
 import { RestaurantCard } from "../components/RestaurantCard";
 
+// services
 import { RestaurantsContext } from "../../../services/restaurants/restaurantsContext";
 
 const SearchContainer = styled(View)`
@@ -20,10 +21,25 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``;
 
+const LoaderContainer = styled(View)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
+const Loader = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+
 export const RestaurantsScreen = () => {
   const { restaurants, isLoading, error } = useContext(RestaurantsContext);
   return (
     <Container>
+      {isLoading && (
+        <LoaderContainer>
+          <Loader size={50} animating={true} color={Colors.blue300} />
+        </LoaderContainer>
+      )}
       <SearchContainer>
         <Searchbar placeholder="Search" />
       </SearchContainer>
